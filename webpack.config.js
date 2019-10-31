025a96f19bdd2ab881256c47916f84ec68bd075f
Puneet Papneja <puneet.papneja@metacube.com>
2019-10-31 14:38:58 +0530

    fixed webpack issue and create menu

--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,49 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-   entry: './src/index.js',
-   output: {
-      path: path.join(__dirname, '/bundle'),
-      filename: 'index_bundle.js'
-   },
-   devServer: {
-      inline: true,
-      port: 8001
-   },
-   module: {
-      rules: [
-         {
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-            query: {
-               presets: ['es2015', 'react']
-            }
-         }
-      ]
-   },
-   plugins:[
-      new HtmlWebpackPlugin({
-         template: './index.html'
-      })
-   ]
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.join(__dirname, '/build'),
+    filename: 'index_bundle.js',
+    publicPath: "/"
+  },
+  mode: process.env.NODE_ENV || 'development',
+  resolve: { modules: [path.resolve(__dirname, 'src'), 'node_modules'] },
+  devServer: {
+    contentBase: path.join(__dirname, 'build'),
+    inline: true,
+    port: 8001,
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/env',
+            '@babel/react'
+
+          ]
+        }
+      },
+      {
+        test: /\.css?$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(pdf|jpg|png|gif|svg|ico)$/,
+        use: [{ loader: 'url-loader' }]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public', 'index.html')
+    })
+  ]
 }
