7339d936a8118ee81c8cbd575803e3ec28fdf4f0
puneetpapneja <puneetpapneja89@gmail.com>
2021-06-03 00:16:23 +0530

    Configure basic webpack configuration

---
+++ react-app/webpack.config.dev.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module:{
+      rules:[
+        {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+          },
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /[\\/]node_modules[\\/]/,
+            use: {
+              loader: 'babel-loader',
+            },
+          },
+      ]
+  },
+  plugins:[
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+          filename: 'index.html',
+          inject: true,
+          template: path.resolve(__dirname,'src','index.html')
+      })
+  ]
+}
